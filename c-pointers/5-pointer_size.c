81e4ffac6644a9f6200147dd79ac63968a2c136f
Atolagbe Aliyah <harliyahatolagbe@gmail.com>
2026-01-17 22:05:52 +0100

    Corrected spacing

--- c-pointers/5-pointer_size.c
+++ c-pointers/5-pointer_size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Size of int* : %zu bytes\n", sizeof(int *));
-    printf("Size of char* : %zu bytes\n", sizeof(char *));
-    printf("Size of float* : %zu bytes\n", sizeof(float *));
-    printf("Size of void* : %zu bytes\n", sizeof(void *));
+    printf("Size of int* : %zu bytes\n", sizeof(int*));
+    printf("Size of char*: %zu bytes\n", sizeof(char*));
+    printf("Size of float*: %zu bytes\n", sizeof(float*));
+    printf("Size of void*: %zu bytes\n", sizeof(void*));
 
     return 0;
 }
